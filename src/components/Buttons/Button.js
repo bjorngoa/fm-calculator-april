61d4d83c4adba624e9ae9a3d807a1e166e229ee1
bjornole-kodehode <93578742+bjornole-kodehode@users.noreply.github.com>
2022-04-26 14:46:41 +0200

    update

--- src/components/Buttons/Button.js
+++ src/components/Buttons/Button.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-export default function Button({ className, value, key, handleClick, e }) {
+export default function Button({ className, value, key, handleClick }) {
   return (
     <button key={key} className={className} onClick={(e) => handleClick(e)}>
       {value}
